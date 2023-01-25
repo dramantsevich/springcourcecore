bf1a4ff066c2dca735e79e26b08ce3b0600b1a0e
Dzmitry Ramantsevich <d.ramantsevich@gmail.com>
2023-01-25 15:55:39 +0300

    lesson 6 - Dependency Injection(setter/getter method's some value)

--- src/main/java/org/example/MusicPlayer.java
+++ src/main/java/org/example/MusicPlayer.java
@@ -3,6 +3,25 @@ package org.example;
 public class MusicPlayer {
     private Music music;
 
+    private String name;
+    private int volume;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getVolume() {
+        return volume;
+    }
+
+    public void setVolume(int volume) {
+        this.volume = volume;
+    }
+
     //IoC
     public MusicPlayer(Music music) {
         this.music = music;
