d1376162dd9a34f255c2181d3ed6ae27c8309be9
Dzmitry Ramantsevich <d.ramantsevich@gmail.com>
2023-01-26 21:54:51 +0300

    lesson 10 - Spring annotations(@Autowired by constructor with 2 beans)

--- src/main/java/org/example/MusicPlayer.java
+++ src/main/java/org/example/MusicPlayer.java
@@ -5,10 +5,17 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class MusicPlayer {
-    @Autowired
-    private Music music;
+    private ClassicalMusic classicalMusic;
+    private RockMusic rockMusic;
 
-    public void playMusic() {
-        System.out.println("Playing: " + music.getSong());
+    @Autowired
+    public MusicPlayer(ClassicalMusic classicalMusic, RockMusic rockMusic) {
+        this.classicalMusic = classicalMusic;
+        this.rockMusic = rockMusic;
+    }
+
+     public void playMusic() {
+        System.out.println("Playing: " + classicalMusic.getSong());
+         System.out.println("Playing: " + rockMusic.getSong());
     }
 }
